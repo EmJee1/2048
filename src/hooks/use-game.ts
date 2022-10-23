ffa3086d37b102937719041eb6b357cface7f118
EmJee <mart-janroeleveld@outlook.com>
2022-10-24 00:44:32 +0200

    Less unnecessary game-state state updates

--- src/hooks/use-game.ts
+++ src/hooks/use-game.ts
@@ -1,22 +1,27 @@
-import { useState } from 'react'
-import { Direction } from '../types'
+import { useEffect, useState } from 'react'
+import { Direction, GameState } from '../types'
 import { getRandomEmptyIndexFromGameState } from '../utils/arrays'
 import { moveGameState } from '../utils/move'
 import { generateInitialGameState } from '../utils/initial'
 import { isEqual } from 'lodash'
 import { storeGameState } from '../utils/persistent-storage'
 
+// TODO: move this to designated util file
+// TODO: make value 90% -> 2, 10% -> 4
+const addCellValue = (gameState: GameState) => {
+  const cellToAdd = getRandomEmptyIndexFromGameState(gameState)
+  const updatedGameState = [...gameState]
+  updatedGameState[cellToAdd] = 2
+  return updatedGameState
+}
+
 const useGame = (columns: number) => {
   const [gameState, setGameState] = useState(generateInitialGameState(columns))
 
-  const addCellValue = () => {
-    const cellToAdd = getRandomEmptyIndexFromGameState(gameState)
-
-    setGameState((gs) => {
-      gs[cellToAdd] = 2
-      return gs
-    })
-  }
+  useEffect(() => {
+    console.log('Saving game-state with value:', gameState)
+    storeGameState(gameState)
+  }, [gameState])
 
   const move = (direction: Direction) => () => {
     const movedGameState = moveGameState(direction, gameState, columns)
@@ -25,9 +30,7 @@ const useGame = (columns: number) => {
       return
     }
 
-    setGameState(movedGameState)
-    addCellValue()
-    storeGameState(gameState)
+    setGameState(addCellValue(movedGameState))
   }
 
   return {
